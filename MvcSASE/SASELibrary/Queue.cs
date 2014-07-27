46fa93287e0b64c65cacda3506455194ca207a76
ingramsh <ingramsh85@gmail.com>
2014-07-27 15:57:20 -0700

    Removed unneeded Usings

--- MvcSASE/SASELibrary/Queue.cs
+++ MvcSASE/SASELibrary/Queue.cs
@@ -3,8 +3,6 @@ using Microsoft.WindowsAzure.Storage.Queue;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SASELibrary
 {
