1bce3696a05d119f2b6149e6b9fab5a91520380e
ingramsh <ingramsh85@gmail.com>
2014-07-27 16:41:33 -0700

    Migrated SASE model to SASELibrary

---
+++ MvcSASE/SASELibrary/DBContext.cs
@@ -0,0 +1,11 @@
+﻿using System.Data.Entity;
+
+namespace SASELibrary
+{
+    public class DBContext : DbContext
+    {
+        public DBContext()
+            : base("SASEDBContext") { }
+        public DbSet<AccountService> Sase { get; set; }
+    }
+}
