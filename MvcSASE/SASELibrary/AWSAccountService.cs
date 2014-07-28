8a9c4ecfc6602f565a937d1674ee44b3d8f46180
ingramsh <ingramsh85@gmail.com>
2014-07-27 18:11:38 -0700

    Created AWSAccountService class

---
+++ MvcSASE/SASELibrary/AWSAccountService.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SASELibrary
+{
+    public class AWSAccountService : AccountService
+    {
+        public override IEnumerable<string> BlobContainerNames()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override BlobInfo BlobInfo(string container, string item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IEnumerable<string> BlobItemNames(string container)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IEnumerable<string> BlobItems(string container)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool CreateContainer(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool CreateQueue(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string DequeueMessage(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool DownloadBlobBlock(string container, string item, string filepath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override byte[] DownloadBlobBytes(string container, string item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override System.IO.Stream DownloadBlobStream(string container, string item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool EnqueueMessage(string name, string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Message PeekMessage(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int QueueCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int QueueMessageCount(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IEnumerable<string> QueueNames()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool UploadBlockBlob(string container, string filepath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool UploadBlockBlobBytes(string container, string name, byte[] file)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool UploadBlockBlobStream(string container, string name, System.IO.Stream file)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
